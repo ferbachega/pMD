a04fadacd3ce0a959cab0a7b5704c61ee9831722
ferbachega <ferbachega@gmail.com>
2019-08-21 18:47:38 -0300

    box and LJ

--- md.py
+++ md.py
@@ -35,15 +35,15 @@ T  = 273.15
 NA = 6.022E23
 m  = (M/1000)/ NA
 dt = 1E-12
-number_of_steps = 100
-
-#parameter
-natoms =  100
+number_of_steps = 1000
+r  = 2.0
 
 
-sigma = math.sqrt((kb * T)/m)
-mu    = 0 
-
+#parameters
+natoms =  5 
+sigma  = math.sqrt((kb * T)/m)
+mu     = 0 
+box    = [(-50,50), (-50, 50), (-50, 50)]  
 
 
 pos = np.random.rand(natoms, 3)
@@ -55,6 +55,52 @@ uz  = (np.random.normal(mu, sigma, natoms))*1E10
 fx = np.zeros(natoms)
 fy = np.zeros(natoms)
 fz = np.zeros(natoms)
+
+
+
+def export_cell (box):
+	""" Function doc 
+	ATOM  1      H   LIG  1        -50.00  -50.00  -50.000   1.00  300.00          H 0000  
+	ATOM  2      H   LIG  1        -50.00   50.00  -50.000   1.00  300.00          H 0000  
+	ATOM  3      H   LIG  1        -50.00  -50.00   50.000   1.00  300.00          H 0000  
+	ATOM  4      H   LIG  1         50.00  -50.00  -50.000   1.00  300.00          H 0000  
+	ATOM  5      H   LIG  1         50.00   50.00  -50.000   1.00  300.00          H 0000  
+	ATOM  6      H   LIG  1         50.00  -50.00   50.000   1.00  300.00          H 0000  
+	ATOM  7      H   LIG  1        -50.00   50.00   50.000   1.00  300.00          H 0000  
+	ATOM  8      H   LIG  1         50.00   50.00   50.000   1.00  300.00          H 0000  
+	p0_0_0 = "ATOM  1      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][0]), str(box[2][0]))
+	p0_1_0 = "ATOM  2      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][1]), str(box[2][0]))
+	p0_0_1 = "ATOM  3      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][0]), str(box[2][1]))
+	p1_0_1 = "ATOM  4      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][0]), str(box[2][0]))
+	p1_1_0 = "ATOM  5      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][1]), str(box[2][0]))
+	p1_0_1 = "ATOM  6      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][0]), str(box[2][1]))
+	p0_1_1 = "ATOM  7      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][1]), str(box[2][1]))
+	p1_1_1 = "ATOM  8      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][1]), str(box[2][1]))
+	"""
+
+	cellout = open('cell_xyz.pdb', 'w')
+	string  = "ATOM  1      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][0]), str(box[2][0]))
+	string += "ATOM  2      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][1]), str(box[2][0]))
+	string += "ATOM  3      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][0]), str(box[2][1]))
+	string += "ATOM  4      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][0]), str(box[2][0]))
+	string += "ATOM  5      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][1]), str(box[2][0]))
+	string += "ATOM  6      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][0]), str(box[2][1]))
+	string += "ATOM  7      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][0]), str(box[1][1]), str(box[2][1]))
+	string += "ATOM  8      H   LIG  1         %s  %s  %s   1.00  300.00          H 0000\n" %(str(box[0][1]), str(box[1][1]), str(box[2][1]))
+	string += "CONECT    1    2    3    4 \n"
+	string += "CONECT    2    7    5      \n"
+	string += "CONECT    3    7    6      \n"
+	string += "CONECT    4    5    6      \n" 
+	string += "CONECT    5    8           \n"
+	string += "CONECT    6    8           \n"
+	string += "CONECT    7    8           \n"
+	cellout.write(string)
+
+
+
+
+
+
 
 def write_xyz_file (pos):
 	""" Function doc """
@@ -90,23 +136,124 @@ def integrate (pos, ux, uy, uz, fx, fy, fz, mass, dt):
 		pos[i][2]  += uz[i] * dt
 		ux[i]      += fz[i] * dt / m
 
+def sphereHitCheck (pos, ux, uy, uz, fx, fy, fz):
+	""" Function doc """
+	for i in range(natoms):
+		for j in range (i+1, natoms):
+			#check X
+			
+			
+			d_x = pos[i][0] - pos[j][0]
+			d_y = pos[i][1] - pos[j][1]
+			d_z = pos[i][2] - pos[j][2]
+
+			dist = (d_x**2 + d_y**2 + d_z**2) 
+			
+			
+			
+
+			#if diff_x <= r:
+			#	ux[i] = -ux[i]
+			#	ux[j] = -ux[j]
+			#
+			#if diff_y <= r:
+			#	uy[i] = -uy[i]
+			#	uy[j] = -uy[j]
+			#
+			#if diff_z <= r:
+			#	uz[i] = -uz[i]
+			#	uz[j] = -uz[j]
+			
+			#print pos[i][0],  pos[i][1], pos[i][2]
+			#pos[i][0]  += ux[i] * dt
+			#ux[i]      += fx[i] * dt / m
+			#
+			#pos[i][1]  += uy[i] * dt
+			#ux[i]      += fy[i] * dt / m
+			#
+			#pos[i][2]  += uz[i] * dt
+			#ux[i]      += fz[i] * dt / m	
+
 
 def wallHitCheck (pos, ux, uy, uz, fx, fy, fz):
 	""" Function doc """
 	
+	for i in range(natoms):
+		#print pos[i][0],  pos[i][1], pos[i][2]
+		
+		pos[i][0]  += ux[i] * dt
+		#'''
+		#walls in X
+		if pos[i][0] >= box[0][1]:
+			diff      =  pos[i][0] - box[0][1]
+			pos[i][0] = pos[i][0]-diff
+			ux[i]     =  -ux[i]
+		else:
+			pass
+			
+		if pos[i][0] <= box[0][0]:
+			diff      =  pos[i][0] - box[0][0]
+			pos[i][0] = pos[i][0]-diff
+			ux[i]     =  -ux[i]
+		else:
+			pass
+		#'''
+
+
+
+		#'''
+		#walls in Y
+		if pos[i][1] >= box[1][1]:
+			diff      =  pos[i][1] - box[1][1]
+			pos[i][1] = pos[i][1]-diff
+			uy[i]     =  -uy[i]
+		else:
+			pass
+			
+		if pos[i][1] <= box[1][0]:
+			diff      =  pos[i][1] - box[1][0]
+			pos[i][1] = pos[i][1]-diff
+			uy[i]     =  -uy[i]
+		else:
+			pass
+
+		#'''
+		
+		
+		
+		#'''
+		#walls in Z
+		if pos[i][2] >= box[2][1]:
+			diff      =  pos[i][2] - box[2][1]
+			pos[i][2] = pos[i][2]-diff
+			uz[i]     =  -uz[i]
+		else:
+			pass
+			
+		if pos[i][2] <= box[2][0]:
+			diff      =  pos[i][2] - box[2][0]
+			pos[i][2] = pos[i][2]-diff
+			uz[i]     =  -uz[i]
+		else:
+			pass
+		#'''
 	pass
 	
 
 
 def run (number_of_steps):
 	""" Function doc """
-
+	export_cell (box)
+	trajout = open('traj.xyz', 'w')
+	
 	for i in range (number_of_steps):
 		
 	
 		forces =  computeForce(pos, m)
 		
 		integrate (pos, ux, uy, uz, fx, fy, fz, m, dt)
+
+		sphereHitCheck(pos, ux, uy, uz, fx, fy, fz)
 
 		wallHitCheck(pos, ux, uy, uz, fx, fy, fz)
 		
